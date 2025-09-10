5dc87cb5b59865735507b407676868270de82104
Eleven <discordklb@gmail.com>
2025-09-10 09:24:18 +0100

    canarysupport

--- install.sh
+++ install.sh
@@ -5,20 +5,24 @@ url="https://raw.githubusercontent.com/shvedes/discord-gruvbox/refs/heads/main/$
 
 path_vesktop="$HOME/.config/vesktop/themes"
 path_vencord="$HOME/.config/Vencord/themes"
+path_vencord_canary="$HOME/.config/VencordCanary/themes"
 
 has_vesktop=$(command -v vesktop)
 has_discord=$(command -v discord)
+has_discord_canary=$(command -v discordcanary)
 
 targets=()
 
-if [ -n "$has_vesktop" ] && [ -n "$has_discord" ]; then
-    targets+=("$path_vesktop" "$path_vencord")
+if [ -n "$has_vesktop" ] && { [ -n "$has_discord" ] || [ -n "$has_discord_canary" ]; }; then
+    targets+=("$path_vesktop" "$path_vencord" "$path_vencord_canary")
 elif [ -n "$has_vesktop" ]; then
     targets+=("$path_vesktop")
 elif [ -n "$has_discord" ]; then
     targets+=("$path_vencord")
+elif [ -n "$has_discord_canary" ]; then
+    targets+=("$path_vencord_canary")
 else
-    echo "Neither vesktop nor vencord is installed. Exiting."
+    echo "Neither vesktop nor vencord (stable or canary) is installed. Exiting."
     exit 1
 fi
 
